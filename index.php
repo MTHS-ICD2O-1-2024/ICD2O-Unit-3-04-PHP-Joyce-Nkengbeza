07fc7206f4b133c7e5555a8c0e1401b93ecd20f3
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-08 16:40:25 +0000

    template

--- index.php
+++ index.php
@@ -1,9 +1,88 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<!-- ICS2O-Unit3-01-HTML-MDL -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Area Of Trapezoid, PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Joyce Nkengbeza" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-iconnew.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32new.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16new.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Area Of Trapezoid In PHP</title>
+</head>
+
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Area Of Trapezoid, PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/trapezoid.svg" alt="Area of Trapezoid Image" />
+      </div>
+      <h3>Formula</h3>
+      <div class="page-content-php">
+        <h2>A = [(a + b) / 2] x h</h2>
+      </div>
+      <div class="page-content-php">Please enter integers for dimensions</div>
+      <br />
+      <div class="page-content-php">
+        <form action="answer.php" method="GET">
+          <p>A Base</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="a-base">
+            <label class="mdl-textfield__label" for="a-base"> A base length ...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <br />
+          <p>B Base</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="b-base">
+            <label class="mdl-textfield__label" for="b-base">B base Length ...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <p>Height</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="height-length">
+            <label class="mdl-textfield__label" for="height-length">Height Length Here ...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Calculate
+          </button>
+        </form>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
